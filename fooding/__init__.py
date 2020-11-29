bfb4a78efc0dcf693b2c0da453baa732fe026757
MaimurSV <asoi4@yandex.ru>
2020-11-29 16:57:59 +0500

    Добавлены модели Meal и MealCategory, инициализированы миграции, таблицы заполнены импортом через DBeaver

--- fooding/__init__.py
+++ fooding/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 
 from fooding.config import Config
-from fooding.models import db
+from fooding.models import db, migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db.init_app(app)
+migrate.init_app(app, db)
+
 
 from fooding.views import *
--- fooding/models.py
+++ fooding/models.py
@@ -1,5 +1,27 @@
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_migrate import Migrate
+
 
 db = SQLAlchemy()
+migrate = Migrate()
+
+
+class Meal(db.Model):
+    __tablename__ = "meals"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(25), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(), nullable=False)
+    picture = db.Column(db.String(), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("meal-categories.id"), nullable=False)
+    categories = db.relationship("MealCategory", back_populates="meals")
+
+
+class MealCategory(db.Model):
+    __tablename__ = "meal-categories"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(25), nullable=False)
+    meals = db.relationship('Meal', back_populates='categories')
+
 
