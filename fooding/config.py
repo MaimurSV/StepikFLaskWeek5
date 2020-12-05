40bf7c9b606c3acb413318b3f165266de4e0c075
MaimurSV <asoi4@yandex.ru>
2020-12-05 21:55:56 +0500

    Отладка отключена

--- fooding/config.py
+++ fooding/config.py
@@ -3,7 +3,7 @@ MIN_PASSWORD_LENGTH = 5
 
 
 class Config:
-    DEBUG = True
+    DEBUG = False
     SECRET_KEY = "secret_key"
     SQLALCHEMY_DATABASE_URI = db_path
     SQLALCHEMY_TRACK_MODIFICATIONS = False
